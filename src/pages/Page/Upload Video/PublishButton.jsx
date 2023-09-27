084d3b87b7d0cde5f1e11f3aa02d375c5196612b
Zsoreign <zrsanchez36@gmail.com>
2023-09-27 16:03:31 -0400

    small changes

--- src/pages/Page/Upload Video/PublishButton.jsx
+++ src/pages/Page/Upload Video/PublishButton.jsx
@@ -4,9 +4,10 @@ import {ReactComponent as UploadIcon } from '../../../assets/Icons/publish.svg';
 function PublishButton(props) {
     const className = props.className;
     const label = props.label;
+    const type = props.type;
         return(
             <div className="upload__button--container">
-                <button className={className} type="button">{label}</button>
+                <button className={className} type={type}>{label}</button>
                 <div className="upload__icon--container"><UploadIcon /></div>
             </div>
         );
--- src/pages/Page/Upload Video/VideoUpload.jsx
+++ src/pages/Page/Upload Video/VideoUpload.jsx
@@ -6,31 +6,35 @@ import {ReactComponent as PublishIcon}from '../../../assets/Icons/publish.svg';
 import axios from "axios";
 
 function VideoUpload () {
+
     //const className = props.className;
     //const alt = props.alt
     const [title, setTitle] = useState('');
     const [description, setDescription] = useState('');
 
     const handleSubmit = async (e) => {
-    e.preventDefault();
+        e.preventDefault();
+        console.log('form submitted!');
     
-    const videoData = {
-        title: title,
-        description: description,
-        image: 'http://localhost:3000/images/default-image.jpg', // Hardcoded Image Path
-        views: 0, 
-        likes: 0, 
+        const videoData = {
+            title: title,
+            description: description,
+            image: 'http://localhost:3000/images/default-image.jpg', // Hardcoded Image Path
+            views: 0, 
+            likes: 0, 
+        };
+        console.log('Video Data: ', videoData);
+
+        try {
+            const response = await axios.post('http://localhost:5000/videos', videoData);
+            console.log('Video added: ', response.data);
+            setTitle('');
+            setDescription('');
+        } catch (error) {
+            console.error('Error adding video: ', error);
+        }
     };
 
-    try {
-        const response = await axios.post('http://localhost:3000/videos', videoData);
-        console.log('Video added: ', response.data);
-        setTitle('');
-        setDescription('');
-    } catch (error) {
-        console.error('Error adding video: ', error);
-        }
-  };
     return (
         <section className="upload__video--section">
             <div className="upload__section--title">
@@ -44,24 +48,25 @@ function VideoUpload () {
                         <img src={videoThumbnail} className='upload__thumbnail' alt="thumbnail"></img>
                     </div>
                 </div>
-                <div className="upload__form--container">
-                    <form className="upload__form" onSubmit={handleSubmit}>
+                <form className="upload__form" onSubmit={handleSubmit}>
+                    <div className="upload__form--container">
                         <div className="form__section">
                             <h3>TITLE YOUR VIDEO</h3>
-                            <textarea id="videoTitle" className="video__title" placeholder="Add a title to your video"  value={title} onChange= {(e) => setTitle(e.target.value)} required></textarea>
+                            <input type="text" id="videoTitle" className="video__title" placeholder="Add a title to your video"  value={title} onChange= {(e) => setTitle(e.target.value)} required></input>
                             <h3>ADD A VIDEO DESCRIPTION</h3>
                             <textarea id="videoDescription" className="video__description" rows="10" placeholder="Add a description to your video" value={description} onChange={(e) => setDescription(e.target.value)} required></textarea>
                         </div>
-                    </form>
-                </div>
-            </div>
-            <div className="buttons_container">  
+                    </div>
+            
+                    <div className="buttons_container">  
                             <div className="button__items--container">
                                 <div className="upload__icon--container"><PublishIcon/></div>
-                                <PublishButton className= "publish__button" label='PUBLISH' />
+                                <PublishButton className= "publish__button" label='PUBLISH' type="submit" />
                             </div>
                             <div className="cancel__container"><h2>CANCEL</h2></div>
-                        </div>  
+                        </div>
+                </form>
+            </div>  
         </section>
     );
 
